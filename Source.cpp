26befbf95a761660c9e4092d00037dfab96c61dc
Vladlen Mykhailovskyi <59488404+vladlenm3@users.noreply.github.com>
2023-01-06 01:14:28 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+// includes the library
+#include <iostream>
+using namespace std;
+
+//main function that has the code
+int main() {
+
+	// prints the message to the console
+	cout << "Hello World!\n";
+
+	system("pause");
+	return 0;
+}
